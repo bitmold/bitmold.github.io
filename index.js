0c9377198d4697a4efd3b3817ae7d15502627ad4
lexi <dsnake@protonmail.com>
2019-04-14 14:23:50 -0400

    rudimentary tab styling

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
   // tab switching functionality 
   $(".nav-tabs a").click(function(){
+  	$('.nav-link').removeClass('active');
+  	$(this.parentElement).addClass('active');
     $(this).tab('show');
   });
 });
