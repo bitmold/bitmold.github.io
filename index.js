5b730dca83b33ec92ad3e39df6c0c53ab52eb491
lexi <dsnake@protonmail.com>
2019-04-16 13:54:58 -0400

    Q&D disable

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
-  // tab switching functionality 
+
+  if (document.location.href.includes('frecnotferc.com')) {
+    document.body.innerHTML = 'Test';
+  }
+
+  // tab switching functionality
   $(".nav-tabs a").click(function(){
   	$('.nav-link').removeClass('active');
   	$(this.parentElement).addClass('active');
